83316b47a53eb28ee9ef9e17007115ff30ed9b6a
Dragon Eye <dragoneyessj64@gmail.com>
2017-11-18 20:48:38 -0500

    Fixed Bug: Arrow dissappearing after lock

--- VR Space Archery TSA/Assets/Scripts/ArrowManager.cs
+++ VR Space Archery TSA/Assets/Scripts/ArrowManager.cs
@@ -1,57 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class ArrowManager : MonoBehaviour {
+public class ArrowManager : MonoBehaviour
+{
 
     public static ArrowManager Instance;
+
     public SteamVR_TrackedObject trackedObj;
 
-    public GameObject arrowPrefab;
+    private GameObject currentArrow;
+
     public GameObject stringAttachPoint;
     public GameObject arrowStartPoint;
     public GameObject stringStartPoint;
 
-    private GameObject currentArrow;
+    public GameObject arrowPrefab;
+
+    public bool Arrow_gravity = false;
 
     private bool isAttached = false;
 
-    private void Awake()
+    void Awake()
     {
         if (Instance == null)
-        {
             Instance = this;
-        }
     }
 
     void OnDestroy()
     {
         if (Instance == this)
-        {
             Instance = null;
-        }
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
         AttachArrow();
         PullString();
-	}
-
-    private void AttachArrow()
-    {
-        if (currentArrow == null)
-        {
-            currentArrow = Instantiate(arrowPrefab);
-            currentArrow.transform.parent = trackedObj.transform;
-            currentArrow.transform.localPosition = new Vector3(0f, 0f, .342f);
-            currentArrow.transform.localRotation = Quaternion.identity;
-        }
     }
 
     private void PullString()
@@ -73,12 +64,12 @@ public class ArrowManager : MonoBehaviour {
     {
         float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
 
-        currentArrow.transform.parent = stringAttachPoint.transform;
-        currentArrow.GetComponent<Arrow>().Fired ();
+        currentArrow.transform.parent = null;
+        currentArrow.GetComponent<Arrow>().Fired();
 
         Rigidbody r = currentArrow.GetComponent<Rigidbody>();
-        r.velocity = currentArrow.transform.forward * 15f * dist;
-        r.useGravity = true;
+        r.velocity = currentArrow.transform.forward * 25f * dist;
+        r.useGravity = Arrow_gravity;
 
         currentArrow.GetComponent<Collider>().isTrigger = false;
 
@@ -88,10 +79,21 @@ public class ArrowManager : MonoBehaviour {
         isAttached = false;
     }
 
-    public void AttachArrowToBow()
+    private void AttachArrow()
     {
-        currentArrow.transform.parent = null;
-        currentArrow.transform.localPosition = arrowStartPoint.transform.localPosition;
+        if (currentArrow == null)
+        {
+            currentArrow = Instantiate(arrowPrefab);
+            currentArrow.transform.parent = trackedObj.transform;
+            currentArrow.transform.localPosition = new Vector3(0f, 0f, .342f);
+            currentArrow.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    public void AttachBowToArrow()
+    {
+        currentArrow.transform.parent = stringAttachPoint.transform;
+        currentArrow.transform.position = arrowStartPoint.transform.position;
         currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
 
         isAttached = true;
